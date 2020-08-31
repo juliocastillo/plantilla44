5636a0aa29b04f73ad07cdd3c6120d2562211715
Julio Castillo <jcastillore@yahoo.es>
2020-08-31 14:14:44 -0600

    agregando fos_user al proyecto plantilla

---
+++ src/Entity/Group.php
@@ -0,0 +1,21 @@
+<?php
+// src/Entity/Group.php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Sonata\UserBundle\Entity\BaseGroup;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="fos_user_group")
+ */
+class Group extends BaseGroup
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    protected $id;
+}
